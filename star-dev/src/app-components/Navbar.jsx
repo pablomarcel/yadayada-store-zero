abe9d864711676254555193108c75e5e982e90fe
pablomarcel <pablomarcel@outlook.com>
2022-06-11 15:51:35 -0700

    added a side drawer component

--- star-dev/src/app-components/Navbar.jsx
+++ star-dev/src/app-components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
         <center><img src="https://fontmeme.com/permalink/220610/2f6798e0ac6e2a6f38e4f3769a208d66.png" alt="Yada Yada"/></center>
         <br/>
         <br />
+
         {/*<div className="links">*/}
         {/*  <a href="/"*/}
         {/*     style={{*/}
