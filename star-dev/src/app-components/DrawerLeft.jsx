bd33b510d86d159bc59592eec22fc994da470b46
pablomarcel <pablomarcel@outlook.com>
2022-06-16 00:33:41 -0700

    Final Commit, replaced permanent drawer with persistent drawer

--- star-dev/src/app-components/DrawerLeft.jsx
+++ star-dev/src/app-components/DrawerLeft.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { makeStyles } from '@mui/styles'
+import React, {useState} from 'react'
+import { makeStyles, styled, useTheme} from '@mui/styles'
 import Drawer from '@mui/material/Drawer'
 import Typography from '@mui/material/Typography'
 import { useNavigate } from 'react-router'
@@ -7,8 +7,11 @@ import {useLocation} from 'react-router-dom'
 import List from '@mui/material/List'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
-import { LocalOffer, Home} from '@mui/icons-material'
-import {ListItemButton} from "@mui/material";
+import { LocalOffer, Home, Menu} from '@mui/icons-material'
+import {IconButton, ListItemButton, Toolbar} from "@mui/material";
+import { ChevronLeft } from '@mui/icons-material'
+import { ChevronRight } from '@mui/icons-material'
+import MuiAppBar from '@mui/material/AppBar';
 
 const drawerWidth = 240
 
@@ -54,6 +57,41 @@ export default function DrawerLeft({ children }) {
   const classes = useStyles()
   const history = useNavigate()
   const location = useLocation()
+  const [open, setOpen] = useState(false)
+  const theme = useTheme()
+
+  const handleDrawerOpen = () =>{
+    setOpen(true)
+  }
+
+  const handleDrawerClose = () =>{
+    setOpen(false)
+  }
+
+  const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+  })(({ theme, open }) => ({
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: `${drawerWidth}px`,
+      transition: theme.transitions.create(['margin', 'width'], {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    }),
+  }));
+
+  const DrawerHeader = styled('div')(({theme}) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0,1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }))
 
   const menuItems = [
     {
@@ -92,15 +130,64 @@ export default function DrawerLeft({ children }) {
   return (
     <div className={classes.root}>
 
+      <AppBar
+        position="fixed"
+        open={open}
+        elevation={0}
+        style={{
+          backgroundColor:"#E0F2F1"
+        }}
+
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 2, ...(open && { display: 'none' }) }}
+            style={{
+              color: "#009688",
+
+            }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap component="div"
+            style={{
+              color:"#009688"
+            }}
+
+          >
+            Menu
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       {/* side drawer */}
+
+
       <Drawer
         color="primary"
         className={classes.drawer}
-        variant="permanent"
+        variant="persistent"
         // classes={{ paper: classes.drawerPaper }}
         classes={{ paper: classes.paper }}
         anchor="left"
+        open={open}
       >
+
+
+
+
+        <DrawerHeader>
+          <IconButton onClick = {handleDrawerClose}>
+            {/*{theme.direction==='ltr' ? <ChevronLeft /> : <ChevronRight/>}*/}
+            {theme.direction==='ltr' ? <ChevronLeft /> : <ChevronRight/>}
+          </IconButton>
+        </DrawerHeader>
         <div style={{
           backgroundColor: "#009688"
         }}>
@@ -109,7 +196,7 @@ export default function DrawerLeft({ children }) {
             className={classes.title}
             color="white"
           >
-            Menu
+            {/*Menu*/}
           </Typography>
         </div>
 
--- star-dev/src/app-components/Navbar.jsx
+++ star-dev/src/app-components/Navbar.jsx
@@ -34,7 +34,10 @@ const Navbar = () => {
   return (
 
     <nav className="navbar">
+
       <div className="navbar-container">
+        <br/>
+        <br/>
         <center><img src="https://fontmeme.com/permalink/220610/2f6798e0ac6e2a6f38e4f3769a208d66.png" alt="Yada Yada"/></center>
         <br/>
         <br />
